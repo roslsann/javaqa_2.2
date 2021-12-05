93490358a7a9da139b18782fe884e2bc8e8d410a
roslsann <sann23@mail.ru>
2021-12-05 17:14:20 +0300

    main.java modified

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 150;
+        int transaction = 1550;
+        int bonus;
+        if (transaction >= 1000) {
+            bonus = transaction / 100;
+        } else {
+            bonus = 0;
+        }
+        int totalBalance = currentBalance + transaction + bonus;
+        System.out.println(bonus + " бонусов начисленно.");
+        System.out.println("Ваш баланс " + totalBalance + " рублей.");
+
+    }
 }
+
